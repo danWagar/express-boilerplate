b1b024b799ba59895259bff590d7aa8ab6a6ab8c
danWagar <dandwagar@gmail.com>
2020-02-08 16:08:19 -0800

    add pg, knex, jwt, user/auth endpoints

--- src/app.js
+++ src/app.js
@@ -4,18 +4,25 @@ const morgan = require('morgan');
 const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config');
+const authRouter = require('./auth/auth-router');
+const userRouter = require('./user/user-router');
 
 const app = express();
 
-const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
-
-app.use(morgan(morganOption));
+app.use(
+  morgan(NODE_ENV === 'production' ? 'tiny' : 'common', {
+    skip: () => NODE_ENV === 'test'
+  })
+);
 app.use(cors());
 app.use(helmet());
 
 app.get('/', (req, res) => {
   res.send('Hello boilerplate');
 });
+
+app.use('/api/auth', authRouter);
+app.use('/api/user', userRouter);
 
 app.use(function errorHandler(error, req, res, next) {
   let response;
